7794f945b7c6fba3419fed597cf3c9096bb1a16d
Mark M. Florida <mflorida@gmail.com>
2020-07-27 23:36:38 -0500

    Added Charter Rewards compiled demo.

---
+++ demos/charter-rewards/precache-manifest.212c93f67eafa71ef183c424c7b9aaaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e2a41a60757d64e1c8f7df8f868324",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f37bce15084a3407b950",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fc4e47b92661b15324e1",
+    "url": "./static/css/main.7565d8d5.chunk.css"
+  },
+  {
+    "revision": "f37bce15084a3407b950",
+    "url": "./static/js/2.2ce0f612.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2ce0f612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4e47b92661b15324e1",
+    "url": "./static/js/main.50d0da0d.chunk.js"
+  },
+  {
+    "revision": "1b9aec3f5bd89a19c004",
+    "url": "./static/js/runtime-main.c099baa4.js"
+  }
+]);
